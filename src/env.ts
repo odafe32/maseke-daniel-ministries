53c47f5207c71fa2c6b39064182d29a163de706e
Godfrey <you@example.com>
2025-12-10 19:43:25 +0100

    Stop tracking env.ts

--- src/env.ts
+++
@@ -1,8 +0,0 @@
-import { z } from 'zod';
-
-const env = z.object({
-  API_URL: z.string().url().default('http://10.220.96.161:8000/api'),
-  APP_ENV: z.string().default('development'),
-});
-
-export const { API_URL, APP_ENV } = env.parse(process.env);
